9a03bc10a4d0a240ce007b2108586d0fb994b47d
K-andy13 <102968948+K-andy13@users.noreply.github.com>
2023-06-27 10:26:21 +0000

    Add files via upload

---
+++ Search.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Search{
+    public static void intro(){
+        System.out.println("Welcome to the world of searching.\nWhat searching algorithm would you like to use?\n\n(1)Linear Search\n(2) Binary\n");
+    }
+    public static Integer linearSearch(ArrayList<Integer> T){
+        //insert implemetation here
+        int soln=0;//assign index of your found key to soln or -1 if key is not found,0 is just a placeholder
+        System.out.println(soln);
+
+        return soln;}
+
+        public static Integer binarySearch(ArrayList<Integer> T){
+            //insert implementation heres
+            int soln=0;//assign index of your found key to soln or -1 if key is not found,0 is just a placeholder
+            System.out.println(soln);
+            return soln;}
+}
---
+++ Sort.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+class Sort{
+    public static void intro(){
+        System.out.println("Welcome to the world of sorting.What sorting algorithm would you like to use?\n\n(1) Bubble Sort\n(2) Selection Sort \n(3)Insertion Sort \n(4)Merge Sort\n(5)Radix Sort \n(6)Quicksort \n" );
+    }
+    public static ArrayList<Integer> bubbleSort(ArrayList<Integer> T){
+        //input implementation here
+        return T;
+    }
+
+    public static ArrayList<Integer> selectionSort(ArrayList<Integer> T){
+                //input implementation here
+
+        return T;
+    }
+    public static ArrayList<Integer> insertionSort(ArrayList<Integer> T){
+                //input implementation here
+
+        return T;
+    }
+    public static ArrayList<Integer> radixSort(ArrayList<Integer> T){
+                //input implementation here
+
+        return T;
+    }
+    public static ArrayList<Integer> quickSort(ArrayList<Integer> T){
+                //input implementation here
+
+        return T;
+    }
+
+}
