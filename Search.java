d2e4248425f8ecc6569d9d638d7717e72c2ecb27
K-andy13 <kmanderson@st.ug.edu.gh>
2023-06-28 09:39:40 +0000

    Updated files

--- Search.java
+++ Search.java
@@ -1,5 +1,32 @@
+import java.util.*;
 class Search{
     public static void intro(){
-        System.out.println("Welcome to the world of searching.\nWhat searching algorithm would you like to use?\n(1)Linear Search\n(2) Binary\n");
+        System.out.println("Welcome to the world of searching.\nWhat searching algorithm would you like to use?\n\n(1)Linear Search\n(2) Binary\n");
     }
+
+     public static void selectAlgo(int x){
+             SearchorSort j=new SearchorSort();
+
+        switch(x){
+            case 1:linearSearch(j.getProblemArr());
+            break;
+            case 2:binarySearch(j.getProblemArr());break;
+            default:for(int p:SearchorSort.problemArr){System.out.println(p);}
+
+        }}
+
+   
+
+    public static int linearSearch(int[] T){
+        //insert implemetation here
+        int soln=0;//assign index of your found key to soln or -1 if key is not found,0 is just a placeholder
+        System.out.println(soln);
+
+        return soln;}
+
+        public static int binarySearch(int[] T){
+            //insert implementation heres
+            int soln=0;//assign index of your found key to soln or -1 if key is not found,0 is just a placeholder
+            System.out.println(soln);
+            return soln;}
 }
--- Sort.java
+++ Sort.java
@@ -1,6 +1,53 @@
+
 class Sort{
+
     public static void intro(){
-        System.out.println("Welcome to the world of sorting.What sorting algorithm would you like to use?\n\n(1) Bubble Sort\n(2) Selection Sort \n(3)Insertion Sort \n(4)Merge Sort\n(5)Radix Sort \n(6)Quicksort \n"
-        );
+        System.out.println("Welcome to the world of sorting.What sorting algorithm would you like to use?\n\n(1) Bubble Sort\n(2) Selection Sort \n(3)Insertion Sort \n(4)Merge Sort\n(5)Radix Sort \n(6)Quicksort \n" );
+    }
+
+    public static void selectAlgo(int x){
+             SearchorSort j=new SearchorSort();
+
+        switch(x){
+            case 1:bubbleSort(j.getProblemArr());break;
+            case 2:selectionSort(j.getProblemArr());break;
+            case 3:insertionSort(j.getProblemArr());break;
+            case 4:mergeSort(j.getProblemArr());break;
+            case 5:radixSort(j.getProblemArr());break;
+            case 6:quickSort(j.getProblemArr());break;
+            default:for(int p:SearchorSort.problemArr){System.out.println(p);}
+
+        }
+    }
+    public static int[] bubbleSort(int[] T){
+        //input implementation here
+        return T;
+    }
+
+    public static int[] selectionSort(int[] T){
+                //input implementation here
+
+        return T;
+    }
+    public static int[] insertionSort(int[] T){
+                //input implementation here
+
+        return T;
+    }
+    public static int[] radixSort(int[] T){
+                //input implementation here
+
+        return T;
+    }
+    public static int[] quickSort(int[] T){
+                //input implementation here
+
+        return T;
+    }
+
+     public static int[] mergeSort(int[] T){
+                //input implementation here
+
+        return T;
     }
 }
