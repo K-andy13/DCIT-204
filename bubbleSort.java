4486941b3254dbf9a75988cd8e3037eaccead427
K-andy13 <kmanderson@st.ug.edu.gh>
2023-06-29 12:19:51 +0000

    dw

--- bubbleSort.java
+++ bubbleSort.java
@@ -4,15 +4,16 @@
 public static void main(String[] args){
     
     int array[] = {3,60,35,2,45,320,5};
+BubbleSort bubbleSortObj=new BubbleSort();
 
-    bubbleSort(array);
+    bubbleSortObj.bubbleSort(array);
         System.out.print("[");
 
     for(int i: array){ System.out.print(i+","); }
       System.out.print("]");
 
 }
-    public static void bubbleSort(int array[]){
+    public void bubbleSort(int array[]){
         for(int i=0; i< array.length - 1; i++){
             for(int j=0; j< array.length -i - 1; j++){
                 if(array[j]>array[j+1]){
